7ad6bc0c25e13ba65a3965f6f570c54a4f320086
Arda Karaduman <akaraduman@gmail.com>
2017-12-08 16:22:25 +0900

    update gass bill

--- tokyo_gas.rb
+++ tokyo_gas.rb
@@ -8,6 +8,9 @@ require 'dotenv/load'
 require_relative 'browser'
 require_relative 'drive'
 
+PDF_FOLDER = ENV['TOKYO_GAS_PDF_FOLDER_KEY']
+CSV_FOLDER = ENV['TOKYO_GAS_CSV_FOLDER_KEY']
+
 class TOKYO_GAS
   attr_accessor :br, :drive
 
@@ -15,6 +18,8 @@ class TOKYO_GAS
     @download_dir = ENV['DOWNLOAD_DIR']
     @br = Browser.new
     @drive = Drive.new ENV['TOKYO_GAS_DRIVE_KEY']
+
+    @bill_page = "https://members.tokyo-gas.co.jp/mytokyogas/mtgmenu/mieru/total.aspx"
   end
 
   def login
@@ -22,11 +27,12 @@ class TOKYO_GAS
     @br.chrome.text_field(name: 'main_2$txtLoginId').set ENV['TOKYO_GAS_USER']
     @br.chrome.text_field(name: 'main_2$txtPassword').set ENV['TOKYO_GAS_PASS']
     @br.chrome.button(name: 'main_2$btnSubmit').click
+    go_to_main
   end
 
   def go_to_main
-    @br.chrome.link(text: "見える").click
-    @br.chrome.link(text: "料金明細を見る ＞").click
+    @br.chrome.goto @bill_page
+    @br.chrome.td(class: "list-more-view").click
   end
 
   def update_sheet(row, key, value)
@@ -35,38 +41,74 @@ class TOKYO_GAS
     @drive.ws.save
   end
 
+  def base_table
+    @br.chrome.table(class: 'billing-all')
+  end
+
   def get_amount(index)
-    @br.chrome.table(class: 'base2').rows[index].cells[1].text
+    base_table.rows[index].cells[1].text
   end
 
   def get_month(index)
-    @br.chrome.table(class: 'base2').rows[index].cells[0].text
+    base_table.rows[index].cells[0].text
+  end
+
+  def pdf_button
+    @br.chrome.link(text: "ガスPDFダウンロード")
+  end
+
+  def csv_button
+    @br.chrome.link(text: "CSVダウンロード")
+  end
+
+  def download_pdf(month)
+    if pdf_button.exists?
+      pdf_button.click
+      sleep 5
+      file = Dir.glob("#{@download_dir}/*.pdf").first
+      FileUtils.mv(file, "./#{month}.pdf")
+      @drive.upload("#{month}.pdf", PDF_FOLDER)
+      File.delete "#{month}.pdf"
+    end
+  end
+
+  def download_csv(month)
+    if csv_button.exists?
+      csv_button.click
+      sleep 5
+      file = Dir.glob("#{@download_dir}/*.csv").first
+
+      csv_file = "#{month}.csv"
+      File.open(csv_file, 'w:UTF-8') do |f|
+        f.write File.open(file, "r:Shift_JIS").read
+      end
+      @drive.upload(csv_file, CSV_FOLDER)
+
+      File.delete file
+      File.delete csv_file
+    end
   end
 
   def download_all(from = nil)
-    binding.pry
     should_download = from ? false : true
 
-    @br.chrome.button(class: "dropdown-toggle").click
-    (1..@br.chrome.table(class: 'base2').rows.count-1).each do |i|
-      month = get_month(i)
+    (1..base_table.rows.count-2).each do |i|
+      table_row = (base_table.rows.count-1-i)
+      month = get_month(table_row)
       if month == from
         should_download = true
         next
       end
 
       if should_download
-        update_sheet(i, month, get_amount(i))
+        update_sheet(i, month, get_amount(table_row))
 
-        @br.chrome.table(class: 'base2').rows[i].cells[2].button.click
-        sleep 3
+#        base_table.rows[table_row].cells[4].link.click
+#        download_pdf(month)
+#        download_csv(month)
+#        go_to_main
+#        sleep 3
 
-        file = "#{month}.html"
-        File.open(file, 'w:UTF-8') do |f|
-          f.write @br.chrome.table(id: 'main-contents').html
-        end
-
-        go_to_main
       end
     end
   end
